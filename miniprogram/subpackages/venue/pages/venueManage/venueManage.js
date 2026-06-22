d49211d892b534545e973bc75091b1ca4434c45e
Accumulation93 <18667663305@163.com>
2026-06-23 01:13:09 +0800

    fix(venue): replace Array.from with hardcoded array for WeChat runtime compatibility
    Array.from() is not available in some WeChat mini-program JS runtimes,
    which caused Page() constructor to fail silently. This prevented all
    methods (onCycleTypeChange, onToggleCycleDay, etc.) from being registered,
    resulting in 'method not found' errors.

--- miniprogram/subpackages/venue/pages/venueManage/venueManage.js
+++ miniprogram/subpackages/venue/pages/venueManage/venueManage.js
@@ -31,7 +31,7 @@ Page({
     // Yearly date picker state
     yearlyPickMonth: 1,
     yearlyPickDay: 1,
-    yearlyDays: Array.from({length: 31}, (_, i) => i + 1)
+    yearlyDays: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
   },
 
   onShow() {
